e353792d5ce7c9ed174237953021439a6d1b451e
xineting <1018494707@qq.com>
2022-09-18 23:18:28 +0800

    Update DAASTFrontedAction.h

--- DAASTFrontedAction.h
+++ DAASTFrontedAction.h
@@ -3,26 +3,32 @@
  *	06/22/2020 pyx : Created.                    *
  *	06/01/2022 pyx : Edited.                     *
 =================================================*/
+#ifndef DAASTFrontendAction_H
+#define DAASTFrontendAction_H
+
+
 
 #include "DAASTConsumer.h"
 #include "DAContext.h"
 #include "DAFileManager.h"
 #include "DAPPCallbacks.h"
 #include "SymbolTable.h"
-
 #include "clang/Frontend/CompilerInstance.h"
 #include "clang/Frontend/FrontendAction.h"
 #include "clang/Tooling/Tooling.h"
 
+
 class DAASTFrontendAction : public clang::ASTFrontendAction {
+
 public:
   ~DAASTFrontendAction();
   bool BeginSourceFileAction(clang::CompilerInstance &ci);
-  std::unique_ptr<clang::ASTConsumer>
-  CreateASTConsumer(clang::CompilerInstance &Compiler, llvm::StringRef InFile);
-
-private:
+  std::unique_ptr<clang::ASTConsumer> CreateASTConsumer(clang::CompilerInstance &Compiler, llvm::StringRef InFile);
   DAContext *getContext(clang::CompilerInstance &ci);
-  DAContext *m_context;
-  DAFileManager *m_fm; // TODO:remove me
+
+  DAContext *m_context = nullptr;
+
 };
+
+
+#endif
